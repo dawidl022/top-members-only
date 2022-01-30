2db4385216ca8130596bb4bc3fd2ef4dbe2e0d55
dawidl022 <domdaw7@gmail.com>
2022-01-30 19:54:08 +0000

    Add user's name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   root to: "posts#index"
   resource :posts, only: [:index, :create, :new]
+
+  devise_scope :user do
+    get '/users', to: redirect('/users/sign_in')
+  end
 end
